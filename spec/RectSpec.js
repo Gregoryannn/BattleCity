fade77815c5cd25917e53ad8c85f70eff1a20cd1
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-07 11:47:49 +0100

    CollisionDetector should fire event when sprite goes out of bounds

--- spec/RectSpec.js
+++ spec/RectSpec.js
@@ -1,4 +1,5 @@
 describe("Rect", function() {
+
     it("default state", function() {
         var rect = new Rect();
         expect(rect.getWidth()).toEqual(1);
@@ -52,6 +53,32 @@ describe("Rect", function() {
             var rect1 = new Rect(0, 0, 4, 4);
             var rect2 = new Rect(-2, 2, 4, 4);
             expect(rect1.intersects(rect2)).toBeTruthy();
+        });
+    });
+
+    describe("#containsWhole", function() {
+        it("test 1", function() {
+            var rect1 = new Rect(0, 0, 10, 10);
+            var rect2 = new Rect(0, 0, 10, 10);
+            expect(rect1.containsWhole(rect2)).toBeTruthy();
+        });
+
+        it("test 2", function() {
+            var rect1 = new Rect(0, 0, 10, 10);
+            var rect2 = new Rect(-1, 0, 10, 10);
+            expect(rect1.containsWhole(rect2)).toBeFalsy();
+        });
+
+        it("test 3", function() {
+            var rect1 = new Rect(0, 0, 10, 10);
+            var rect2 = new Rect(3, 3, 3, 3);
+            expect(rect1.containsWhole(rect2)).toBeTruthy();
+        });
+
+        it("test 3", function() {
+            var rect1 = new Rect(0, 0, 10, 10);
+            var rect2 = new Rect(8, 3, 3, 3);
+            expect(rect1.containsWhole(rect2)).toBeFalsy();
         });
     });
 });
