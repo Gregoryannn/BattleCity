def11cdc24d13e7aa7c466f7bb04b256258ca66a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-08 11:40:45 +0100

    Bullet should subscribe

--- src/Bullet.js
+++ src/Bullet.js
@@ -1,13 +1,14 @@
 function Bullet(eventManager) {
     Sprite.call(this, eventManager);
-}
-Bullet.subclass(Sprite);
-
-Bullet.Event = {};
-Bullet.Event.DESTROYED = 'Bullet.Event.DESTROYED';
-
-Bullet.prototype.notify = function(event) {
+    eventManager.addSubscriber(this, [CollisionDetector.Event.OUT_OF_BOUNDS]);
+  }
+  
+  Bullet.subclass(Sprite);
+  Bullet.Event = {};
+  Bullet.Event.DESTROYED = 'Bullet.Event.DESTROYED';
+  Bullet.prototype.notify = function (event) {
     if (event.name == CollisionDetector.Event.OUT_OF_BOUNDS && event.sprite === this) {
-        this._eventManager.fireEvent({ 'name': Bullet.Event.DESTROYED });
+      this._eventManager.fireEvent({'name': Bullet.Event.DESTROYED});
+      this._eventManager.fireEvent({'name': Sprite.Event.DESTROYED, 'sprite': this});
     }
-};
+  };
