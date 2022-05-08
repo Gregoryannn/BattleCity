9edfcc2a274b1eb46dcf50284147e569c5721d90
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-08 11:44:50 +0100

    feat: event Manager can remove subscribers

--- spec/SpriteSpec.js
+++ spec/SpriteSpec.js
@@ -1,12 +1,12 @@
 describe("Sprite", function() {
     var eventManager, sprite;
 
-    beforeEach(function () {
+    beforeEach(function() {
         eventManager = new EventManager();
         sprite = new Sprite(eventManager);
-      });
+    });
 
-      describe("initial state", function () {
+    describe("initial state", function() {
         it("direction should be Right", function() {
             expect(sprite.getDirection()).toEqual(Sprite.Direction.RIGHT);
         });
@@ -15,21 +15,30 @@ describe("Sprite", function() {
             expect(sprite.getSpeed()).toEqual(0);
         });
     });
-    
-    describe("#move", function () {
-        it("should fire event when moved", function () {
-          spyOn(eventManager, 'fireEvent');
-          sprite.setSpeed(1);
-          sprite.move();
-          expect(eventManager.fireEvent).toHaveBeenCalledWith({
-            'name': Sprite.Event.MOVED,
-            'sprite': sprite});
+
+    describe("#move", function() {
+        it("should fire event when moved", function() {
+            spyOn(eventManager, 'fireEvent');
+            sprite.setSpeed(1);
+            sprite.move();
+            expect(eventManager.fireEvent).toHaveBeenCalledWith({
+                'name': Sprite.Event.MOVED,
+                'sprite': sprite
+            });
         });
-    
-        it("shouldn't fire event if not moved", function () {
-          spyOn(eventManager, 'fireEvent');
-          sprite.move();
-          expect(eventManager.fireEvent).not.toHaveBeenCalled();
+
+        it("shouldn't fire event if not moved", function() {
+            spyOn(eventManager, 'fireEvent');
+            sprite.move();
+            expect(eventManager.fireEvent).not.toHaveBeenCalled();
         });
-      });
+    });
+
+    it("#destroy", function() {
+        spyOn(eventManager, 'removeSubscriber');
+        spyOn(eventManager, 'fireEvent');
+        sprite.destroy();
+        expect(eventManager.removeSubscriber).toHaveBeenCalledWith(sprite);
+        expect(eventManager.fireEvent).toHaveBeenCalledWith({ 'name': Sprite.Event.DESTROYED, 'sprite': sprite });
+    });
 });
--- src/Bullet.js
+++ src/Bullet.js
@@ -1,14 +1,18 @@
 function Bullet(eventManager) {
     Sprite.call(this, eventManager);
     eventManager.addSubscriber(this, [CollisionDetector.Event.OUT_OF_BOUNDS]);
-  }
-  
-  Bullet.subclass(Sprite);
-  Bullet.Event = {};
-  Bullet.Event.DESTROYED = 'Bullet.Event.DESTROYED';
-  Bullet.prototype.notify = function (event) {
+}
+Bullet.subclass(Sprite);
+Bullet.Event = {};
+Bullet.Event.DESTROYED = 'Bullet.Event.DESTROYED';
+
+Bullet.prototype.notify = function(event) {
     if (event.name == CollisionDetector.Event.OUT_OF_BOUNDS && event.sprite === this) {
-      this._eventManager.fireEvent({'name': Bullet.Event.DESTROYED});
-      this._eventManager.fireEvent({'name': Sprite.Event.DESTROYED, 'sprite': this});
+        this.destroy();
     }
-  };
+};
+
+Bullet.prototype.destroy = function() {
+    Sprite.prototype.destroy.call(this);
+    this._eventManager.fireEvent({ 'name': Bullet.Event.DESTROYED });
+};
