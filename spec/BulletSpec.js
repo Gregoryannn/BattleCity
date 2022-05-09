63f0292357e52af46eb35b795784b9bd18926d47
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-09 11:55:41 +0100

    Bullet should be destroyed when collides with a wall

--- spec/BulletSpec.js
+++ spec/BulletSpec.js
@@ -1,18 +1,44 @@
 describe("Bullet", function() {
-    it("should subscribe", function() {
-        var eventManager = new EventManager();
-        spyOn(eventManager, 'addSubscriber');
-        var tank = new Tank(eventManager);
-        var bullet = new Bullet(eventManager, tank);
-        expect(eventManager.addSubscriber).toHaveBeenCalledWith(bullet, [CollisionDetector.Event.OUT_OF_BOUNDS]);
+    var eventManager, tank, bullet;
+
+    beforeEach(function() {
+        eventManager = new EventManager();
+        tank = new Tank(eventManager);
+        bullet = new Bullet(eventManager, tank);
     });
 
-    it("should fire event when goes out of bounds", function() {
-        var eventManager = new EventManager();
+    it("#destroy", function() {
         spyOn(eventManager, 'fireEvent');
-        var tank = new Tank(eventManager);
-        var bullet = new Bullet(eventManager, tank);
+        bullet.destroy();
+        expect(eventManager.fireEvent).toHaveBeenCalledWith({ 'name': Bullet.Event.DESTROYED, 'bullet': bullet, 'tank': tank });
+    });
+
+    it("should be destroyed when goes out of bounds", function() {
+        spyOn(bullet, 'destroy');
         bullet.notify({ 'name': CollisionDetector.Event.OUT_OF_BOUNDS, 'sprite': bullet });
-        expect(eventManager.fireEvent).toHaveBeenCalledWith({ 'name': Bullet.Event.DESTROYED, 'tank': tank });
+        expect(bullet.destroy).toHaveBeenCalled();
+    });
+
+    it("should be destroyed when collides with a wall", function() {
+        var wall = new Wall(eventManager);
+        spyOn(bullet, 'destroy');
+        bullet.notify({
+            'name': CollisionDetector.Event.COLLISION,
+            'initiator': bullet,
+            'sprite': wall
+        });
+        expect(bullet.destroy).toHaveBeenCalled();
     });
 });
+
+
+describe("Bullet", function() {
+            it("should subscribe", function() {
+                    var eventManager = new EventManager();
+                    spyOn(eventManager, 'addSubscriber');
+                    var tank = new Tank(eventManager);
+                    var bullet = new Bullet(eventManager, tank);
+                    expect(eventManager.addSubscriber).toHaveBeenCalledWith(bullet, [CollisionDetector.Event.OUT_OF_BOUNDS]);
+                    [CollisionDetector.Event.OUT_OF_BOUNDS, CollisionDetector.Event.COLLISION]); pect(eventManager.fireEvent).toHaveBeenCalledWith({ 'name': Bullet.Event.DESTROYED, 'bullet': bullet, 'tank': tank });
+
+            });
