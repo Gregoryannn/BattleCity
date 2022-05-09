379af5f2fac690f2c0a7af3ab5c2d6704fbac759
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-09 11:07:10 +0100

    add bullet images

--- src/Bullet.js
+++ src/Bullet.js
@@ -7,6 +7,7 @@ function Bullet(eventManager, tank) {
 Bullet.subclass(Sprite);
 Bullet.Event = {};
 Bullet.Event.DESTROYED = 'Bullet.Event.DESTROYED';
+
 Bullet.prototype.notify = function(event) {
     if (event.name == CollisionDetector.Event.OUT_OF_BOUNDS && event.sprite === this) {
         this.destroy();
@@ -16,4 +17,12 @@ Bullet.prototype.notify = function(event) {
 Bullet.prototype.destroy = function() {
     Sprite.prototype.destroy.call(this);
     this._eventManager.fireEvent({ 'name': Bullet.Event.DESTROYED, 'tank': this._tank });
+};
+
+Bullet.prototype.getImage = function() {
+    return 'bullet_' + this._direction;
+};
+
+Bullet.prototype.draw = function(ctx) {
+    ctx.drawImage(ImageManager.getImage(this.getImage()), this._x, this._y);
 };
