3f1c1170cc6a56cfdcffb6b8de372cac830cd09e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-10 09:43:23 +0100

    add Construction

--- spec/SpriteSpec.js
+++ spec/SpriteSpec.js
@@ -6,14 +6,12 @@ describe("Sprite", function() {
         sprite = new Sprite(eventManager);
     });
 
-    describe("initial state", function() {
-        it("direction should be Right", function() {
-            expect(sprite.getDirection()).toEqual(Sprite.Direction.RIGHT);
-        });
-
-        it("speed should be 0", function() {
-            expect(sprite.getSpeed()).toEqual(0);
-        });
+    it("initial state", function() {
+        expect(sprite.getDirection()).toEqual(Sprite.Direction.RIGHT);
+        expect(sprite.getPrevDirection()).toEqual(Sprite.Direction.RIGHT);
+        expect(sprite.getSpeed()).toEqual(0);
+        expect(sprite.isDestroyed()).toBeFalsy();
+        expect(sprite.isTurn()).toBeFalsy();
     });
 
     describe("#move", function() {
@@ -49,12 +47,60 @@ describe("Sprite", function() {
         });
     });
 
-    it("#destroy", function() {
+    it("#doDestroy", function() {
         spyOn(eventManager, 'removeSubscriber');
         spyOn(eventManager, 'fireEvent');
-        sprite.destroy();
+        sprite.doDestroy();
         expect(eventManager.removeSubscriber).toHaveBeenCalledWith(sprite);
         expect(eventManager.fireEvent).toHaveBeenCalledWith({ 'name': Sprite.Event.DESTROYED, 'sprite': sprite });
+    });
+
+    describe("#update", function() {
+        it("not destroyed", function() {
+            spyOn(sprite, 'doDestroy');
+            spyOn(sprite, 'move');
+            sprite.update();
+            expect(sprite.doDestroy).not.toHaveBeenCalled();
+            expect(sprite.move).toHaveBeenCalled();
+        });
+
+        it("destroyed", function() {
+            spyOn(sprite, 'doDestroy');
+            spyOn(sprite, 'move');
+            sprite.destroy();
+            sprite.update();
+            expect(sprite.doDestroy).toHaveBeenCalled();
+            expect(sprite.move).not.toHaveBeenCalled();
+        });
+    });
+
+    describe("#isTurn", function() {
+        it("test 1", function() {
+            sprite.setDirection(Sprite.Direction.RIGHT);
+            sprite.move();
+            sprite.setDirection(Sprite.Direction.UP);
+            expect(sprite.isTurn()).toBeTruthy();
+        });
+
+        it("test 2", function() {
+            sprite.setDirection(Sprite.Direction.RIGHT);
+            sprite.move();
+            expect(sprite.isTurn()).toBeFalsy();
+        });
+    });
+
+    describe("#getPrevDirection", function() {
+        it("test 1", function() {
+            sprite.setDirection(Sprite.Direction.RIGHT);
+            sprite.setDirection(Sprite.Direction.LEFT);
+            expect(sprite.getPrevDirection()).toEqual(Sprite.Direction.RIGHT);
+        });
+
+        it("test 2", function() {
+            sprite.setDirection(Sprite.Direction.DOWN);
+            sprite.setDirection(Sprite.Direction.RIGHT);
+            expect(sprite.getPrevDirection()).toEqual(Sprite.Direction.DOWN);
+        });
     });
 });
 
--- src/Level.js
+++ src/Level.js
@@ -1,25 +1,16 @@
 function Level(eventManager) {
-    Rect.call(this);
 
-    this._x = 32;
-    this._y = 16;
-    this._w = 416; // 13 tiles * 32 px
-    this._h = 416;
+    Gamefield.call(this, eventManager);
 
-    this._painter = new Painter(eventManager);
-    this._updater = new Updater(eventManager);
-
-    var bounds = new Rect(this._x, this._y, this._w, this._h);
-    var collisionDetector = new CollisionDetector(eventManager, bounds);
     var tank = new Tank(eventManager);
     tank.setPosition(new Point(this._x, this._y));
     tank.setNormalSpeed(2);
     tank.setBulletSize(8);
     tank.setBulletSpeed(4);
 
-    var tankController = new TankController(eventManager, tank);
-    var bulletFactory = new BulletFactory(eventManager);
-    var explosionFactory = new ExplosionFactory(eventManager);
+    new TankController(eventManager, tank);
+    new BulletFactory(eventManager);
+    new ExplosionFactory(eventManager);
 
     new BrickWall(eventManager).setPosition(new Point(this._x + 32, this._y + 32));
     new BrickWall(eventManager).setPosition(new Point(this._x + 32, this._y + 48));
@@ -30,13 +21,4 @@ function Level(eventManager) {
     new SteelWall(eventManager).setPosition(new Point(this._x + 96, this._y + 48));
 }
 
-Level.subclass(Rect);
-Level.prototype.update = function() {
-    this._updater.update();
-};
-Level.prototype.draw = function(ctx) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(this._x, this._y, this._w, this._h);
-
-    this._painter.draw(ctx);
-};
+Level.subclass(Gamefield);
