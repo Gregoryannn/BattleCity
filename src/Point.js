0637695b57a5513dd656a9ac622929f68c7a2c20
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-11 00:17:42 +0100

    Builder: add build functions

--- src/Point.js
+++ src/Point.js
@@ -2,23 +2,30 @@ function Point(x, y) {
     this._x = x === undefined ? 0 : x;
     this._y = y === undefined ? 0 : y;
 }
-Point.prototype.getX = function() {
+Point.prototype.getX = function () {
     return this._x;
 };
 
-Point.prototype.getY = function() {
-    return this._y;
-};
-Point.prototype.getPosition = function() {
-    return new Point(this._x, this._y);
+Point.prototype.setX = function (x) {
+    this._x = x;
 };
 
-Point.prototype.setPosition = function(position) {
+Point.prototype.getY = function () {
+    return this._y;
+};
+
+Point.prototype.setY = function (y) {
+    this._y = y;
+};
+
+Point.prototype.getPosition = function () {
+    return new Point(this._x, this._y);
+};
+Point.prototype.setPosition = function (position) {
     this._x = position.getX();
     this._y = position.getY();
 };
-
-Point.prototype.setXY = function(x, y) {
+Point.prototype.setXY = function (x, y) {
     this._x = x;
     this._y = y;
 };
