4b3768f66dede1aedc3ef46852eb52656ff3b181
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-16 09:35:57 +0100

    rename Explosion to BulletExplosion

---
+++ spec/BulletExplosionSpec.js
@@ -0,0 +1,12 @@
+describe("BulletExplosion", function () {
+    it("should be destroyed when animation is finished", function () {
+        var eventManager = new EventManager();
+        var explosion = new BulletExplosion(eventManager);
+        spyOn(explosion, 'destroy');
+        explosion.setFrames([1]);
+        explosion.update();
+        expect(explosion.destroy).not.toHaveBeenCalled();
+        explosion.update();
+        expect(explosion.destroy).toHaveBeenCalled();
+    });
+});
