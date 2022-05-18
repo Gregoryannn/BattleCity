5c2a7babe888f48a47edba4d31213d4a763d9248
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-18 15:00:21 +0100

    fix tests

--- spec/TankStateNormalSpec.js
+++ spec/TankStateNormalSpec.js
@@ -4,6 +4,7 @@ describe("TankStateNormal", function () {
     beforeEach(function () {
         eventManager = new EventManager();
         tank = new Tank(eventManager);
+        tank.setTrackAnimationDuration(1);
         state = new TankStateNormal(tank);
     });
 
@@ -11,38 +12,66 @@ describe("TankStateNormal", function () {
         it("animate when tank is moving", function () {
             tank.setSpeed(1);
             expect(state.getTrackFrame()).toEqual(1);
-            state.updateTrackFrame();
+            state.updateTrackAnimation();
             expect(state.getTrackFrame()).toEqual(2);
-            state.updateTrackFrame();
+            state.updateTrackAnimation();
             expect(state.getTrackFrame()).toEqual(1);
         });
         it("don't animate when tank is not moving", function () {
             tank.setSpeed(0);
             expect(state.getTrackFrame()).toEqual(1);
-            state.updateTrackFrame();
+            state.updateTrackAnimation();
             expect(state.getTrackFrame()).toEqual(1);
-            state.updateTrackFrame();
+            state.updateTrackAnimation();
             expect(state.getTrackFrame()).toEqual(1);
         });
+    });
+
+    it("flash animation", function () {
+        tank.startFlashing();
+        state.setFlashDuration(3);
+        expect(state.isFlashed()).toBeTruthy();
+        state.updateFlash();
+        expect(state.isFlashed()).toBeTruthy();
+        state.updateFlash();
+        expect(state.isFlashed()).toBeTruthy();
+        state.updateFlash();
+        expect(state.isFlashed()).toBeFalsy();
+        state.updateFlash();
+        expect(state.isFlashed()).toBeFalsy();
+        state.updateFlash();
+        expect(state.isFlashed()).toBeFalsy();
+        state.updateFlash();
+        expect(state.isFlashed()).toBeTruthy();
     });
 
     describe("#getImage", function () {
         it("RIGHT", function () {
             tank.setDirection(Sprite.Direction.RIGHT);
-            state.setTrackFrame(1);
             expect(state.getImage()).toEqual('tank_player1_right_1');
         });
         it("LEFT", function () {
+            tank.toNormalSpeed();
             tank.setDirection(Sprite.Direction.LEFT);
-            state.setTrackFrame(2);
+            state.update();
             expect(state.getImage()).toEqual('tank_player1_left_2');
+        });
+        it("flashed", function () {
+            tank.setDirection(Sprite.Direction.RIGHT);
+            tank.startFlashing();
+            state.setFlashed(true);
+            expect(state.getImage()).toEqual('tank_player1_right_1_f');
         });
     });
 
-    it("#update", function () {
-        spyOn(state, 'updateTrackFrame');
-        state.update();
-        expect(state.updateTrackFrame).toHaveBeenCalled();
+    describe("#update", function () {
+        it("#update", function () {
+            spyOn(state, 'updateTrackAnimation');
+            spyOn(state, 'updateFlash');
+            state.update();
+            expect(state.updateTrackAnimation).toHaveBeenCalled();
+            expect(state.updateFlash).toHaveBeenCalled();
+        });
     });
 
     it("#canMove", function () {
