c573a32d114c76e180c07cea1799d7e1604b3634
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-23 11:00:32 +0100

    add MainMenuScene

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -1,15 +1,21 @@
 function SceneManager() {
     this._scene = null;
 }
-
 SceneManager.prototype.setScene = function (scene) {
     this._scene = scene;
+};
+
+SceneManager.prototype.getScene = function () {
+    return this._scene;
+};
+
+SceneManager.prototype.toMainMenuScene = function () {
+    this._scene = new MainMenuScene();
 };
 
 SceneManager.prototype.update = function () {
     this._scene.update();
 };
-
 SceneManager.prototype.draw = function (ctx) {
     this._scene.draw(ctx);
 };
