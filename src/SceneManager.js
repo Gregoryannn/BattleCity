071a31c53e21bd77aca5bf3cc4d04349262489eb
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-24 09:04:43 +0100

    integrate Level

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -8,29 +8,28 @@ SceneManager.prototype.setScene = function (scene) {
 SceneManager.prototype.getScene = function () {
     return this._scene;
 };
+SceneManager.prototype.toMainMenuScene = function (arrived) {
+    this._eventManager.removeAllSubscribers();
+    this._scene = new MainMenuScene(this, this._eventManager);
 
- SceneManager.prototype.toMainMenuScene = function (arrived) {
-        this._eventManager.removeAllSubscribers();
-        this._scene = new MainMenuScene(this, this._eventManager);
+    if (arrived) {
+        this._scene.nextMenuItem();
+        this._scene.arrived();
+    }
+};
 
-        if (arrived) {
-            this._scene.nextMenuItem();
-            this._scene.arrived();
-        }
-    };
+SceneManager.prototype.toLevelScene = function () {
+    this._eventManager.removeAllSubscribers();
+    this._scene = new Level(this, this._eventManager);
+};
 
-    SceneManager.prototype.toLevelScene = function () {
-        this._eventManager.removeAllSubscribers();
-    };
-
-    SceneManager.prototype.toConstructionScene = function () {
-        this._eventManager.removeAllSubscribers();
-        this._scene = new Construction(this, this._eventManager);
-    };
-
-    SceneManager.prototype.update = function () {
-        this._scene.update();
-    };
-    SceneManager.prototype.draw = function (ctx) {
-        this._scene.draw(ctx);
-    };
+SceneManager.prototype.toConstructionScene = function () {
+    this._eventManager.removeAllSubscribers();
+    this._scene = new Construction(this, this._eventManager);
+};
+SceneManager.prototype.update = function () {
+    this._scene.update();
+};
+SceneManager.prototype.draw = function (ctx) {
+    this._scene.draw(ctx);
+};
