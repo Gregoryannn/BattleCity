7e85f3d8cbf5db46d16591a30f6113047dc51963
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-26 15:07:11 +0100

    implement tank bonus

--- src/Player.js
+++ src/Player.js
@@ -3,8 +3,7 @@ function Player() {
     this._score = 0;
     this.resetTanks();
 }
-
-Player.prototype.resetTanks = function () {
+Player.prototype.resetTanks = function() {
     this._tanks = {};
     this._tanks[Tank.Type.BASIC] = 0;
     this._tanks[Tank.Type.FAST] = 0;
@@ -12,42 +11,38 @@ Player.prototype.resetTanks = function () {
     this._tanks[Tank.Type.ARMOR] = 0;
     this._tanksCount = 0;
 };
-
-Player.prototype.setEventManager = function (eventManager) {
+Player.prototype.setEventManager = function(eventManager) {
     this._eventManager = eventManager;
     this._eventManager.addSubscriber(this, [
         PointsFactory.Event.POINTS_CREATED,
         Tank.Event.PLAYER_DESTROYED,
+        PowerUpHandler.Event.TANK,
         Tank.Event.ENEMY_DESTROYED
     ]);
 };
-
-Player.prototype.notify = function (event) {
-        if (event.name == PointsFactory.Event.POINTS_CREATED) {
-            this._score += event.points.getValue();
+Player.prototype.notify = function(event) {
+    if (event.name == PointsFactory.Event.POINTS_CREATED) {
+        this._score += event.points.getValue();
+    } else if (event.name == Tank.Event.PLAYER_DESTROYED) {
+        this._lives--;
+    } else if (event.name == PowerUpHandler.Event.TANK) {
+        this._lives++;
+    } else if (event.name == Tank.Event.ENEMY_DESTROYED) {
+        if (event.tank.getValue() > 0) {
+            this._tanks[event.tank.getType()]++;
+            this._tanksCount++;
         }
-        else if (event.name == Tank.Event.PLAYER_DESTROYED) {
-            this._lives--;
-        }
-        else if (event.name == Tank.Event.ENEMY_DESTROYED) {
-            if (event.tank.getValue() > 0) {
-                this._tanks[event.tank.getType()]++;
-                this._tanksCount++;
-            }
-        }
-    };
-
-    Player.prototype.getScore = function () {
-        return this._score;
-    };
-
-    Player.prototype.getLives = function () {
-        return this._lives;
-    };
-
-    Player.prototype.getTanks = function (type) {
-        return this._tanks[type];
-    };
-    Player.prototype.getTanksCount = function () {
-        return this._tanksCount;
-    };
+    }
+};
+Player.prototype.getScore = function() {
+    return this._score;
+};
+Player.prototype.getLives = function() {
+    return this._lives;
+};
+Player.prototype.getTanks = function(type) {
+    return this._tanks[type];
+};
+Player.prototype.getTanksCount = function() {
+    return this._tanksCount;
+};
