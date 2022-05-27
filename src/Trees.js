bd640c6421f451d5b5377b48e4a44362331e1b9b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-27 11:50:47 +0100

    Trees: set z-index to 1

--- src/Trees.js
+++ src/Trees.js
@@ -1,13 +1,12 @@
 function Trees(eventManager) {
     Sprite.call(this, eventManager);
+    this._zIndex = 1;
 }
 
 Trees.subclass(Sprite);
-
-Trees.prototype.getClassName = function () {
+Trees.prototype.getClassName = function() {
     return 'Trees';
 };
-
-Trees.prototype.draw = function (ctx) {
+Trees.prototype.draw = function(ctx) {
     ctx.drawImage(ImageManager.getImage('trees'), this._x, this._y);
 };
