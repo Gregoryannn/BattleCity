5a0372f9cbae56507aae0b749b2fe903e69a2dd2
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-27 16:39:38 +0100

    add powerup_pick sound

--- src/PowerUpHandler.js
+++ src/PowerUpHandler.js
@@ -8,54 +8,58 @@ PowerUpHandler.Event = {};
 PowerUpHandler.Event.FREEZE = 'PowerUpHandler.Event.FREEZE';
 PowerUpHandler.Event.SHOVEL_START = 'PowerUpHandler.Event.SHOVEL_START';
 PowerUpHandler.Event.TANK = 'PowerUpHandler.Event.TANK';
-
 PowerUpHandler.HELMET_DURATION = 345;
-
-PowerUpHandler.prototype.setSpriteContainer = function(container) {
+PowerUpHandler.prototype.setSpriteContainer = function (container) {
     this._spriteContainer = container;
 };
-PowerUpHandler.prototype.notify = function(event) {
+PowerUpHandler.prototype.notify = function (event) {
     if (event.name == PowerUp.Event.DESTROYED) {
         this.handle(event.powerUp);
     }
 };
-PowerUpHandler.prototype.handle = function(powerUp) {
+
+PowerUpHandler.prototype.handle = function (powerUp) {
+    SoundManager.play("powerup_pick");
+
     if (powerUp.getType() == PowerUp.Type.GRENADE) {
         this.handleGrenade();
-    } else if (powerUp.getType() == PowerUp.Type.HELMET) {
+    }
+    else if (powerUp.getType() == PowerUp.Type.HELMET) {
         this.handleHelmet(powerUp.getPlayerTank());
-    } else if (powerUp.getType() == PowerUp.Type.TIMER) {
+    }
+    else if (powerUp.getType() == PowerUp.Type.TIMER) {
         this.handleTimer();
-    } else if (powerUp.getType() == PowerUp.Type.SHOVEL) {
+    }
+    else if (powerUp.getType() == PowerUp.Type.SHOVEL) {
         this.handleShovel();
-    } else if (powerUp.getType() == PowerUp.Type.STAR) {
+    }
+    else if (powerUp.getType() == PowerUp.Type.STAR) {
         this.handleStar(powerUp.getPlayerTank());
-    } else if (powerUp.getType() == PowerUp.Type.TANK) {
+    }
+    else if (powerUp.getType() == PowerUp.Type.TANK) {
         this.handleTank();
     }
 };
-
-PowerUpHandler.prototype.handleGrenade = function() {
-    this._spriteContainer.getEnemyTanks().forEach(function(tank) {
+PowerUpHandler.prototype.handleGrenade = function () {
+    this._spriteContainer.getEnemyTanks().forEach(function (tank) {
         tank.setValue(0);
         tank.destroy();
     });
 };
-PowerUpHandler.prototype.handleHelmet = function(playerTank) {
+PowerUpHandler.prototype.handleHelmet = function (playerTank) {
     var state = new TankStateInvincible(playerTank);
     state.setStateDuration(PowerUpHandler.HELMET_DURATION);
     playerTank.setState(state);
 };
-PowerUpHandler.prototype.handleTimer = function() {
+PowerUpHandler.prototype.handleTimer = function () {
     this._eventManager.fireEvent({ 'name': PowerUpHandler.Event.FREEZE });
 };
-PowerUpHandler.prototype.handleShovel = function() {
+PowerUpHandler.prototype.handleShovel = function () {
     this._eventManager.fireEvent({ 'name': PowerUpHandler.Event.SHOVEL_START });
 };
-PowerUpHandler.prototype.handleStar = function(playerTank) {
+PowerUpHandler.prototype.handleStar = function (playerTank) {
     playerTank.upgrade();
 };
-
-PowerUpHandler.prototype.handleTank = function() {
+PowerUpHandler.prototype.handleTank = function () {
     this._eventManager.fireEvent({ 'name': PowerUpHandler.Event.TANK });
 };
