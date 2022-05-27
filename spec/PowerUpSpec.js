40fcb16e9fbb59ba820772f37a8937f49cf5ef7b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-27 16:45:31 +0100

    add PowerUpEvent

--- spec/PowerUpSpec.js
+++ spec/PowerUpSpec.js
@@ -1,15 +1,17 @@
-describe("PowerUp", function() {
-    it("should subscribe", function() {
+describe("PowerUp", function () {
+    it("should subscribe", function () {
         var eventManager = new EventManager();
         spyOn(eventManager, 'addSubscriber');
         var powerUp = new PowerUp(eventManager);
-        expect(eventManager.addSubscriber).toHaveBeenCalledWith(powerUp, [CollisionDetector.Event.COLLISION, EnemyFactory.Event.ENEMY_CREATED]);
+        expect(eventManager.addSubscriber).toHaveBeenCalledWith(powerUp,
+            [CollisionDetector.Event.COLLISION, EnemyFactory.Event.ENEMY_CREATED]);
     });
 
-    describe("#notify", function() {
-        describe("CollisionDetector.Event.COLLISION", function() {
-            it("player", function() {
+    describe("#notify", function () {
+        describe("CollisionDetector.Event.COLLISION", function () {
+            it("player", function () {
                 var eventManager = new EventManager();
+                spyOn(eventManager, 'fireEvent');
                 var powerUp = new PowerUp(eventManager);
                 spyOn(powerUp, 'destroy');
                 var player = new Tank(eventManager);
@@ -20,10 +22,11 @@ describe("PowerUp", function() {
                 });
                 expect(powerUp.destroy).toHaveBeenCalled();
                 expect(powerUp.getPlayerTank()).toEqual(player);
+                expect(eventManager.fireEvent).toHaveBeenCalledWith({ 'name': PowerUp.Event.PICK, 'powerUp': powerUp });
             });
         });
 
-        it("EnemyFactory.Event.ENEMY_CREATED", function() {
+        it("EnemyFactory.Event.ENEMY_CREATED", function () {
             var eventManager = new EventManager();
             var enemy = new Tank(eventManager);
             enemy.makeEnemy();
@@ -35,7 +38,7 @@ describe("PowerUp", function() {
         });
     });
 
-    it("#destroyHook", function() {
+    it("#destroyHook", function () {
         var eventManager = new EventManager();
         spyOn(eventManager, 'fireEvent');
         var powerUp = new PowerUp(eventManager);
--- src/PowerUp.js
+++ src/PowerUp.js
@@ -1,17 +1,21 @@
 function PowerUp(eventManager) {
     Sprite.call(this, eventManager);
     this._eventManager.addSubscriber(this, [CollisionDetector.Event.COLLISION, EnemyFactory.Event.ENEMY_CREATED]);
+
     this._w = Globals.UNIT_SIZE;
     this._h = Globals.UNIT_SIZE;
+
     this._type = PowerUp.Type.GRENADE;
     this._blinkTimer = new BlinkTimer(7);
     this._value = 500;
     this._playerTank = null;
 }
-
 PowerUp.subclass(Sprite);
+
 PowerUp.Event = {};
 PowerUp.Event.DESTROYED = 'PowerUp.Event.DESTROYED';
+PowerUp.Event.PICK = 'PowerUp.Event.PICK';
+
 PowerUp.Type = {};
 PowerUp.Type.GRENADE = 'grenade';
 PowerUp.Type.HELMET = 'helmet';
@@ -19,50 +23,51 @@ PowerUp.Type.SHOVEL = 'shovel';
 PowerUp.Type.STAR = 'star';
 PowerUp.Type.TANK = 'tank';
 PowerUp.Type.TIMER = 'timer';
-PowerUp.prototype.setType = function(type) {
+PowerUp.prototype.setType = function (type) {
     this._type = type;
 };
-PowerUp.prototype.getType = function() {
+PowerUp.prototype.getType = function () {
     return this._type;
 };
-PowerUp.prototype.setValue = function(value) {
+PowerUp.prototype.setValue = function (value) {
     this._value = value;
 };
-PowerUp.prototype.getValue = function() {
+PowerUp.prototype.getValue = function () {
     return this._value;
 };
-PowerUp.prototype.draw = function(ctx) {
+PowerUp.prototype.draw = function (ctx) {
     if (this._blinkTimer.isVisible()) {
         ctx.drawImage(ImageManager.getImage(this.getImage()), this._x, this._y);
     }
 };
-PowerUp.prototype.updateHook = function() {
+PowerUp.prototype.updateHook = function () {
     this._blinkTimer.update();
 };
-PowerUp.prototype.getImage = function() {
+PowerUp.prototype.getImage = function () {
     return 'powerup_' + this._type;
 };
-PowerUp.prototype.notify = function(event) {
+PowerUp.prototype.notify = function (event) {
     if (this._collidedWithPlayer(event)) {
         this._playerTank = event.initiator;
+        this._eventManager.fireEvent({ 'name': PowerUp.Event.PICK, 'powerUp': this });
         this.destroy();
-    } else if (event.name == EnemyFactory.Event.ENEMY_CREATED) {
+    }
+    else if (event.name == EnemyFactory.Event.ENEMY_CREATED) {
         if (event.enemy.isFlashing()) {
             this.destroy();
         }
     }
 };
-
-PowerUp.prototype.destroyHook = function() {
+PowerUp.prototype.destroyHook = function () {
     this._eventManager.fireEvent({ 'name': PowerUp.Event.DESTROYED, 'powerUp': this });
 };
-PowerUp.prototype.setPlayerTank = function(tank) {
+PowerUp.prototype.setPlayerTank = function (tank) {
     this._playerTank = tank;
 };
-PowerUp.prototype.getPlayerTank = function() {
+PowerUp.prototype.getPlayerTank = function () {
     return this._playerTank;
 };
-PowerUp.prototype._collidedWithPlayer = function(event) {
+PowerUp.prototype._collidedWithPlayer = function (event) {
     if (event.name != CollisionDetector.Event.COLLISION) {
         return false;
     }
