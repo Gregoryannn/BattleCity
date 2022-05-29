d038f0a12326d07ed5f93d5c6ad4d6393aa404a9
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-29 11:52:10 +0100

    LoadingScene: clear screen before drawing text

--- src/LoadingScene.js
+++ src/LoadingScene.js
@@ -2,8 +2,6 @@ function LoadingScene(sceneManager) {
     this._sceneManager = sceneManager;
     this._loadingProgress = 0;
 }
-
-
 LoadingScene.prototype.update = function () {
     this._loadingProgress = ImageManager.getLoadingProgress();
     if (this._loadingProgress == 100) {
@@ -12,6 +10,9 @@ LoadingScene.prototype.update = function () {
 };
 
 LoadingScene.prototype.draw = function (ctx) {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
     ctx.fillStyle = "#ffffff";
     ctx.fillText("LOADING " + ("" + this._loadingProgress).lpad(" ", 3) + "%", 160, 240);
 };
